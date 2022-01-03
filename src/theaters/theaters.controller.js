2ea61b489a411d89df96802a316cf1b8d495c802
sarahaddae <81703827+sarahaddae@users.noreply.github.com>
2022-01-02 20:03:25 -0500

    Adding files

---
+++ src/theaters/theaters.controller.js
@@ -0,0 +1,20 @@
+const service = require("./theaters.service");
+const boundary = require("../errors/asyncErrorBoundary");
+
+
+
+async function list(req, res) {
+  const movieId = req.params.movie_id;
+  let data;
+  if (movieId) {
+    data = await service.listTheatersByMovieId(movieId);
+  } else {
+    data = await service.list();
+  }
+  res.json({ data });
+}
+
+
+module.exports = {
+    list: boundary(list),
+}
